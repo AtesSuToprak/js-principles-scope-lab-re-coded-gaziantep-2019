d789bd69c6ca40b6cec71ea37e73e5f87056b888
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-07 05:32:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = 'BOB';
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function verwriteBestCustomer() {
+  bestCustomer = 'hakan';
+}
+
